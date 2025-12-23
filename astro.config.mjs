9c7f91735f42f189443ac6b876acef9fc5a2fcf2
keith1101 <anhhuybui2006@gmail.com>
2025-12-23 10:19:26 +0700

    chore: add GitHub Actions workflow for deploying to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://keith1101.github.io",
+  base: "/huyit1101",
   vite: {
     plugins: [tailwindcss()],
   },
